983c84187ac4310f2ae3ee9439cfece5016598b1
Jingjo_D <kitty.username@hotmail.com>
2019-05-13 00:30:49 +0700

    first

--- README.md
+++ README.md
@@ -1,65 +1,4 @@
-This project was bootstrapped with Create React App for Chrome Extension.
-
-## Screenshot, when you click on the extension in Chrome
-![Screenshot](https://s20.postimg.cc/na5j4c80t/react.png)
-
-
-## Folder Structure
-
-After creation, your project should look like this:
-
-```
-my-app/
-  README.md
-  node_modules/
-  package.json
-  public/
-    index.html
-    favicon.ico
-    logo.svg
-    manifest.json
-  src/
-    App.css
-    App.js
-    App.test.js
-    index.css
-    index.js
-    logo.svg
-```
-
-For the project to build, **these files must exist with exact filenames**:
-
-* `public/index.html` is the page template;
-* `src/index.js` is the JavaScript entry point.
-
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](#running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-
-
+THANK REPO `xxx`
 ### Chrome Manifest v2
 
 ```json
@@ -74,12 +13,7 @@ Your app is ready to be deployed!
     "default_popup": "index.html",
     "default_title": "Open the popup"
   },
-  "icons": {
-    "16": "logo.svg",
-    "48": "logo.svg",
-    "128": "logo.svg"
-  },
   "permissions": [
   ]
 }
-```
+
--- src/App.js
+++ src/App.js
@@ -1,19 +1,97 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {CopyToClipboard} from 'react-copy-to-clipboard'
+
+const newObj = (newData) =>{
+  //console.log(JSON.parse(newData))
+  
+  newData = JSON.parse(newData)
+
+  let nodeDataArray = newData.nodeDataArray.map((item)=>{
+    //let zOrder = (item.source != '../img/symbol/original/116.png') ? 20 : 2
+    let DataSource = (item.source != '../img/symbol/original/116.png') ? "3" : "1"
+    let datafield = (item.source != '../img/symbol/original/116.png') ? "STATUS_STATE" : "CUR_VALUE"
+    if(item.category == 'valve')
+      return { 
+          ...item,
+          DataSource,
+          datafield
+      }
+    if(item.category == 'static_text' || item.category == 'text_nomodel' || item.category == 'HyperLink')
+    {
+      let format = '###0.0000 U'
+      return {
+        ...item,
+        format,
+      }
+    }
+    else{
+      return {
+        ...item
+      }
+    }
+   
+    })
+    return {
+      ...newData,
+      nodeDataArray
+    }
+   
+    
+
+}
+
+
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      oldjson: null,
+      newjson: null,
+      copied: false
+    }
+  }
+
+  textAreaHandle = (even) => {
+    this.setState({
+      oldjson: even.target.value,
+      copied: false
+    })
+  }
+
+  ConvertHandle = () => {
+    //console.log(newObj(this.state.jsonOld))
+    let NewData = newObj(this.state.oldjson)
+    this.setState({
+      newjson: NewData
+    })
+ 
+   }
+
+   copyToClipboard = (e) => {
+    this.textArea.select();
+    document.execCommand('copy');
+    this.setState({
+      copied: true
+    })
+    // This is just personal preference.
+    // I prefer to not show the the whole text area selected.
+    e.target.focus();
+
+  }
+ 
+
+
   render() {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <textarea placeholder="ใส่ข้อมูล CODE" style={{borderRadius: 10,margin:5}}  onChange={this.textAreaHandle}></textarea>
+        <textarea placeholder="OUTPUT" style={{borderRadius: 10,margin:5, display: 'none'}} ref={(textarea) => this.textArea = textarea} value={(this.state.newjson) ? JSON.stringify(this.state.newjson) : ""}></textarea>
+        <button className="btn" onClick={this.ConvertHandle}>{(this.state.newjson) ? "เรียบร้อย" : "แปลงข้อมูล"}</button><br/> 
+        <button className="btn" onClick={this.copyToClipboard}>{(this.state.copied) ? "เรียบร้อย" : "ก็อบปี้ข้อมูล"}</button><br/> 
       </div>
     );
   }
